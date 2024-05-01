057f8a7594b4fa9fb5066ca8b3f1d00f50cd743e
Julia L. Wang <wang.julia08@gmail.com>
2024-05-01 02:54:53 -0400

    update doppelganger

---
+++ 03.Code/DoppelGANger/README.md
@@ -0,0 +1,7 @@
+# DoppelGANger Running Instructions
+
+Code for running the synthetic data generation experiments from https://github.com/fjxmlzn/DoppelGANger.git. Hyperparameters for training can be found in Table 5 of the thesis paper. 
+
+Workflow:
+- 'training' folder contains code used for the training of the model itself, run the file 'main.py'. 
+- 'generating_data' folder contains code to run after the model has been trained to generate synthetic data, run the file 'main_generate_data.py'. 
---
+++ 03.Code/DoppelGANger/generating_data/config_generate_data.py
@@ -0,0 +1,55 @@
+import os
+
+config = {
+    "scheduler_config": {
+        "gpu": ["0"],
+        "config_string_value_maxlen": 700,
+        "result_root_folder": os.path.join("..", "results"),
+        "scheduler_log_file_path": "scheduler_generate_data.log",
+        "log_file": "worker_generate_data.log",
+        "force_rerun": True
+    },
+
+    "global_config": {
+        "batch_size": 200,
+        "vis_freq": 200,
+        "vis_num_sample": 5,
+        "d_rounds": 1,
+        "g_rounds": 1,
+        "num_packing": 1,
+        "noise": True,
+        "feed_back": False,
+        "g_lr": 0.001,
+        "d_lr": 0.001,
+        "d_gp_coe": 10.0,
+        "gen_feature_num_layers": 1,
+        "gen_feature_num_units": 100,
+        "gen_attribute_num_layers": 3,
+        "gen_attribute_num_units": 100,
+        "disc_num_layers": 5,
+        "disc_num_units": 200,
+        "initial_state": "random",
+
+        "attr_d_lr": 0.001,
+        "attr_d_gp_coe": 10.0,
+        "g_attr_d_coe": 1.0,
+        "attr_disc_num_layers": 5,
+        "attr_disc_num_units": 200,
+
+        "generate_num_train_sample": 50000,
+        "generate_num_test_sample": 50000
+    },
+
+    "test_config": [
+        {
+            "dataset": ["czech"],
+            "epoch": [400],
+            "run": [0, 1, 2],
+            "sample_len": [10],
+            "extra_checkpoint_freq": [5],
+            "epoch_checkpoint_freq": [1],
+            "aux_disc": [False],
+            "self_norm": [False]
+        }
+    ]
+}
